f81f54b401f05254445baaccb59e60ad28e4e9fc
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-18 15:16:35 +0530

    print_header

--- installer.py
+++ installer.py
@@ -3,3 +3,10 @@ import os
 import sys
 import subprocess
 import platform
+
+def print_header():
+    print("=" * 70)
+    print("PLAGIARISM CHECKER - ONE-CLICK INSTALLER")
+    print("Desktop Application Setup")
+    print("=" * 70)
+    print()
