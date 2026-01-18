0a2197a2a9dd6341e230521019f513dd861266a3
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-18 14:58:49 +0530

    Center window

--- main.py
+++ main.py
@@ -185,8 +185,12 @@ def get_sample_database() -> List[Dict]:
 def main():
     root = tk.Tk()
     app = PlagiarismCheckerApp(root)
-    
-    
+    root.update_idletasks()
+    width = root.winfo_width()
+    height = root.winfo_height()
+    x = (root.winfo_screenwidth() // 2) - (width // 2)
+    y = (root.winfo_screenheight() // 2) - (height // 2)
+    root.geometry(f'{width}x{height}+{x}+{y}')
     
     root.mainloop()
 
